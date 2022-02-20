bbef7f7731d1acce48723b79fc12e3bfff61679e
MohamedAbdelfattah99 <67230191+MohamedAbdelfattah99@users.noreply.github.com>
2022-02-20 09:36:57 +0200

    cart-products

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -12,8 +12,11 @@ import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/porduct-details/porduct-details.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CarStatusComponent } from './components/car-status/car-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 const routes:Routes=[
+
+  {path:'cart-details', component:CartDetailsComponent},
 
   {path:'products/:id', component:ProductDetailsComponent},
 
@@ -35,7 +38,8 @@ const routes:Routes=[
     ProductCategoryMenuComponent,
     SearchComponent,
     ProductDetailsComponent,
-    CarStatusComponent
+    CarStatusComponent,
+    CartDetailsComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
